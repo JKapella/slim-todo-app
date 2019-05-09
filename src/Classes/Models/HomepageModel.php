985fc4ebcef60e03e12ea84995dcf0d6a6298c76
Jkapella <jvkapella@gmail.com>
2019-05-09 13:42:49 +0100

    created new method on model to set a db item as completed

--- src/Classes/Models/HomepageModel.php
+++ src/Classes/Models/HomepageModel.php
@@ -22,8 +22,15 @@ class HomepageModel
 
     public function addToDo($message)
     {
-        $query= $this->db->prepare("INSERT INTO `todo_table` (`message`) VALUES (:message);");
+        $query = $this->db->prepare("INSERT INTO `todo_table` (`message`) VALUES (:message);");
         $query->bindParam(':message', $message);
+        $query->execute();
+    }
+
+    public function completeTodo($id)
+    {
+        $query = $this->db->prepare("UPDATE `todo_table` SET `completed` = '1' WHERE `id` = :id;");
+        $query->bindParam(':id', $id);
         $query->execute();
     }
 }
